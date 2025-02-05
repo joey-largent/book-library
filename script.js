b2dace8034b184d23e5746f988499e7218f6e666
joey-largent <177733313+joey-largent@users.noreply.github.com>
2025-02-05 14:19:54 +0100

    add function and array storage

--- script.js
+++ script.js
@@ -5,31 +5,52 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
-};
-
+}
 
 const table = document.querySelector('table');
 
-document.querySelector('.addBook').addEventListener('click', () => {
-    const newBook = document.createElement('tr');
+function addBookToLib() {
+    const newBook = new Book("Title", "Author", 0, "unread");
+    myLibrary.push(newBook);
+
+    const newRow = document.createElement('tr');
+    newRow.setAttribute('data-index', myLibrary.length - 1);
 
     const title = document.createElement('td');
     title.textContent = 'Title';
     title.setAttribute('contenteditable', 'true');
+    title.addEventListener('input', () => {
+        let index = parseInt(newRow.getAttribute('data-index'));
+        myLibrary[index].title = title.textContent;
+        console.log(myLibrary);
+    });
 
     const author = document.createElement('td');
     author.textContent = 'Author';
     author.setAttribute('contenteditable', 'true');
+    author.addEventListener('input', () => {
+        let index = parseInt(newRow.getAttribute('data-index'));
+        myLibrary[index].author = author.textContent;
+        console.log(myLibrary);
+    });
 
     const pages = document.createElement('td');
     pages.textContent = '0';
     pages.setAttribute('contenteditable', 'true');
     pages.addEventListener('input', () => {
-        pages.textConent = pages.textContent.replace(/[^0-9]/g, "");
+        pages.textContent = pages.textContent.replace(/[^0-9]/g, "");
+        let index = parseInt(newRow.getAttribute('data-index'));
+        myLibrary[index].pages = pages.textContent;
+        console.log(myLibrary);
     });
 
     const statusCell = document.createElement('td');
     const status = document.createElement('select');
+    status.addEventListener('change', () => {
+        let index = parseInt(newRow.getAttribute('data-index'));
+        myLibrary[index].status = status.value;
+        console.log(myLibrary);
+    });
 
     status.innerHTML = `
         <option value="unread">Unread</option>
@@ -42,13 +63,17 @@ document.querySelector('.addBook').addEventListener('click', () => {
     remove.classList.add('remove-btn');
     remove.textContent = 'x';
 
-    newBook.appendChild(title);
-    newBook.appendChild(author);
-    newBook.appendChild(pages);
+    newRow.appendChild(title);
+    newRow.appendChild(author);
+    newRow.appendChild(pages);
     statusCell.appendChild(status);
-    newBook.appendChild(statusCell);
+    newRow.appendChild(statusCell);
     removeCell.appendChild(remove);
-    newBook.appendChild(removeCell);
+    newRow.appendChild(removeCell);
 
-    table.appendChild(newBook);
+    table.appendChild(newRow);
+}
+
+document.querySelector('.addBook').addEventListener('click', () => {
+   addBookToLib();
 });
